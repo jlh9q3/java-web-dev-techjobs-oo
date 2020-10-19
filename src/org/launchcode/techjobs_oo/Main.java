7829f1ea403bbcf185510b5e37854666ae93aef9
jess <mini_super_jess@yahoo.com>
2020-10-19 17:58:50 -0500

    updated dry code

--- src/org/launchcode/techjobs_oo/Main.java
+++ src/org/launchcode/techjobs_oo/Main.java
@@ -9,6 +9,7 @@ public class Main {
         Job job2 = new Job("Web Developer", new Employer("LaunchCode"), new Location("St. Louis"), new PositionType("Front-end developer"), new CoreCompetency("JavaScript"));
         Job job3 = new Job("Ice cream taster", new Employer(""), new Location("Home"), new PositionType("UX"), new CoreCompetency("Taste"));
 
+        //jobs 20
 
         ArrayList<Job> jobs = new ArrayList<>();
         jobs.add(job1);
